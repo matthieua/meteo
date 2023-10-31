ea6525d626d8d08741d518df6be4bd1968417fa4
Matt Delac <matthieu.aussaguel@gmail.com>
2023-10-31 15:37:24 +0000

    Add the basic search engine (no API)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
